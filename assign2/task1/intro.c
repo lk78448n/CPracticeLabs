91af7a40485e87b5adef3d8abb7c383a407b1663
lk78448n <lk78448n@pace.edu>
2022-03-06 18:30:43 -0500

    test

--- assign2/task1/intro.c
+++ assign2/task1/intro.c
@@ -5,12 +5,13 @@
 
 int main()
 {
-  float r, a;
+  float r, a, b;
 
   printf("Enter radius (in inches):\n");
   scanf("%f", &r);
 
   a = PI * r * r;
+  b = PI * 2 * r;
 
   printf("Circle's area is %3.2f (sq in).\n", a);
 }
